12eedf48e75c7c547c7b518be080f3d408b7a4f7
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-03-16 22:57:55 +0100

    tests should cover most ofthe usecases now

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,8 +15,16 @@ def test_python_pip_packages_installed(host):
     assert apt_package_python3.is_installed
     assert apt_package_pip.is_installed
 
+def test_pip_installed_latest(host):
+    """Validate pip is installed and up-to-date"""
+    pip_packages_list = host.pip.get_packages(pip_path='pip')
+    pip_outdated_list = host.pip.get_outdated_packages(pip_path='pip')
+    assert 'pip' in pip_packages_list
+    assert 'pip' not in pip_outdated_list
+
 def test_packages_not_installed(host):
-    """Validate vim is installed"""
-    pip_packages_list = host.pip_package.get_packages(pip_path='pip')
-    print(pip_packages_list)
-    assert True == False
+    """Validate docker, yamllint and vault-cli are not installed"""
+    pip_packages_list = host.pip.get_packages(pip_path='pip')
+    assert 'docker' not in pip_packages_list
+    assert 'yamllint' not in pip_packages_list
+    assert 'vault-cli' not in pip_packages_list
--- molecule/with_custom_packages/tests/test_default.py
+++ molecule/with_custom_packages/tests/test_default.py
@@ -1,5 +1,6 @@
 """Role testing files using testinfra."""
-import json
+from packaging import version
+
 
 def test_hosts_file(host):
     """Validate /etc/hosts file."""
@@ -15,11 +16,22 @@ def test_python_pip_packages_installed(host):
     assert apt_package_python3.is_installed
     assert apt_package_pip.is_installed
 
-def test_packages_not_installed(host):
-    """Validate vim is installed"""
-    pip_packages_list = host.pip_package.get_packages(pip_path='pip')
-    assert pip_packages_list['pip']
-    assert pip_packages_list['docker']
-    assert pip_packages_list['zeubi']
-    print(pip_packages_list)
+def test_pip_installed_latest(host):
+    """Validate pip is installed and up-to-date"""
+    pip_packages_list = host.pip.get_packages(pip_path='pip')
+    pip_outdated_list = host.pip.get_outdated_packages(pip_path='pip')
+    assert 'pip' in pip_packages_list
+    assert 'pip' not in pip_outdated_list
 
+def test_packages_installed(host):
+    """Validate docker, yamllint and vault-cli are installed"""
+    pip_packages_list = host.pip.get_packages(pip_path='pip')
+    pip_outdated_list = host.pip.get_outdated_packages(pip_path='pip')
+    assert 'docker' in pip_packages_list
+    assert 'docker' not in  pip_outdated_list
+    assert 'yamllint' in pip_packages_list
+    assert 'yamllint' in  pip_outdated_list
+    assert version.parse(pip_packages_list['yamllint']['version']) == version.parse("1.24")
+    assert 'vault-cli' in pip_packages_list
+    assert 'vault-cli' in  pip_outdated_list
+    assert version.parse(pip_packages_list['vault-cli']['version']) < version.parse("3.1.0")
